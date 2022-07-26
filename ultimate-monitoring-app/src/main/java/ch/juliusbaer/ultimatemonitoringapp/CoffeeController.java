bacc0050e4ad2ed5b5d7f35a6fe938a801cbf81f
zjaskm <zjaskm@bbcag.ch>
2022-07-26 21:46:35 +0200

    Added docker composer for grafana and prometheus

--- ultimate-monitoring-app/src/main/java/ch/juliusbaer/ultimatemonitoringapp/CoffeeController.java
+++ ultimate-monitoring-app/src/main/java/ch/juliusbaer/ultimatemonitoringapp/CoffeeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController {
+public class CoffeeController {
 
     @Autowired
     private CoffeeService coffeeService;
